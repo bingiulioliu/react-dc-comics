6828d928b6e96d31b9af0f69aea3b7eefadfe8f9
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-27 17:05:10 +0200

    ADD Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
+import Navbar from "./components/Navbar";
+
 
 function App() {
   
 
   return (
     <>
-      <h1>Ciao</h1>
+      <Navbar/>
+      
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+
+function Navbar() {
+    return <navbar>
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid">
+                <img src="/img/dc-logo.png" alt="" />
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link " aria-current="page" href="#">CHARACTERS</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link active" href="#">COMICS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">MOVIES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">TV</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">GAMES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">COLLECTIBLES</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">VIDEOS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FANS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">NEWS</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">SHOP</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </navbar>;
+}
+
+export default Navbar;
