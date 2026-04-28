8347742a87d4312554ee13a2bd5857bf4ff7c045
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-28 15:24:59 +0200

    ADD ciclo per lista nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,16 @@
 
 function Navbar() {
+    const navList = ['Characters', 'Comics', 'Movies', 'Tv', 'Games', 'Collectibles', 'Videos', 'Fans', 'News', 'Shop'];
+    let jsxNavList = [];
+
+    // Ciclo for per stendere la lista 
+    for (let i = 0; i < navList.length; i++) {
+        const tempList = navList[i].toUpperCase();
+        jsxNavList.push(<li class="nav-item">
+            <a class="nav-link " aria-current="page" href="#">{tempList}</a>
+        </li>)
+    }
+
     return <navbar>
         <nav class="navbar navbar-expand-lg ">
             <div class="container-fluid">
@@ -9,6 +20,8 @@ function Navbar() {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ms-auto">
+                        {jsxNavList}
+                        {/*
                         <li class="nav-item">
                             <a class="nav-link " aria-current="page" href="#">CHARACTERS</a>
                         </li>
@@ -39,6 +52,7 @@ function Navbar() {
                         <li class="nav-item">
                             <a class="nav-link" href="#">SHOP</a>
                         </li>
+                        */}
                     </ul>
                 </div>
             </div>
