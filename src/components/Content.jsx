f885a4e6bcc47ea66c2bc468817b843505ed9e6d
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-28 16:56:39 +0200

    ADD main structure

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,26 @@
 import style from './Content.module.css'
+import comics from './data/comics';
+
+
 
 function Content() {
-    return <div className={`bg-dark d-flex align-items-center justify-content-center ${style.content}`}>
-        <img className={`${style.jumboImg} img-fluid w-100`} src="./img/jumbotron.jpg" alt="" />
-        
+    let jsxComicsList = [];
+    return <div className={`bg-dark ${style.content}`}>
+                <div className={style.jumbotronWrapper}>
+                    <img className={`${style.jumboImg} img-fluid w-100`} src="./img/jumbotron.jpg" alt="" />
+                <div>
+
+                <div className='container position-relative'>
+                    <button className={style.btnSeries}>CURRENT SERIES</button>
+                    <div className='row'>
+
+                    </div>
+                    <div className='text-center'>
+                        <button className={style.btnLoadMore}>LOAD MORE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
 }
