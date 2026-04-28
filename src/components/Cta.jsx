6dac5c4d059badaf42ba0804d7751562647cf86a
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-28 18:09:12 +0200

    Social listati e renderizzati

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,6 +1,41 @@
 import style from './Content.module.css'
 
 function Cta() {
+
+    const socialLinks = [
+    {
+        id: 1,
+        url: "/img/footer-facebook.png",
+        social: "Facebook"
+    },
+    {
+        id: 2,
+        url: "/img/footer-twitter.png",
+        social: "Twitter"
+    },
+    {
+        id: 3,
+        url: "/img/footer-youtube.png",
+        social: "YouTube"
+    },
+    {
+        id: 4,
+        url: "/img/footer-pinterest.png",
+        social: "Pinterest"
+    },
+    {
+        id: 5,
+        url: "/img/footer-periscope.png",
+        social: "Periscope"
+    }
+    ];
+
+    const socialLinksJsx = socialLinks.map(link =>{
+        const {id, url, social} = link;
+        return <img key={id} src={url} alt={social} className=""/>
+    })
+
+
     return <>
         <section className="bg-dark py-4">
             <div className="container d-flex justify-content-between align-items-center">
@@ -12,12 +47,15 @@ function Cta() {
 
                 <div className="d-flex align-items-center">
                     <h5 className="text-primary fw-bold mb-0 me-4">FOLLOW US</h5>
-                    <div className="d-flex align-items-center">
+                    <div className="d-flex align-items-center gap-2">
+                        {socialLinksJsx}
+                        {/*
                         <img src="/img/footer-facebook.png" alt="" className="me-3"/>
                         <img src="/img/footer-twitter.png" alt="" className="me-3"/>
                         <img src="/img/footer-youtube.png" alt="" className="me-3"/>
                         <img src="/img/footer-pinterest.png" alt="" className="me-3"/>
                         <img src="/img/footer-periscope.png" alt="" />
+                        */}
                     </div>
                 </div>
 
